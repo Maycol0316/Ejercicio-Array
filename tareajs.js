2417ce0b37230e88c10a43c7a94896b4682d540e
Maycol <maycolatila@gmail.com>
2020-07-02 12:02:21 -0500

    array

---
+++ tareajs.js
@@ -0,0 +1,11 @@
+var matriz = [['p',0,0,0,0],[0,'e',0,0,0],[0,0,'r',0,0],[0,0,0,'r',0],[0,0,0,0,'o ']]
+
+console.log(matriz[0][0],matriz[1][1],matriz[2][2],matriz[3][3],matriz[4][4])
+
+var matriz = [['p',0,0,0,'c'],[0,'e',0,'a',0],[0,0,'r',0,0],[0,'n',0,'r',0],['e',0,0,0,'o ']]
+
+console.log(matriz[0][0],matriz[1][1],matriz[2][2],matriz[3][3],matriz[4][4],matriz[0][4],matriz[1][3],matriz[2][2],matriz[3][1],matriz[4][0])
+
+
+var matriz = [['p',0,0,0,[1,2,3,4]],[0,'e',0,false,0],[0,0,'r',0,1],[1.1,0,0,'r',0],[0,0,0,0,'o ']]
+console.log(matriz[0][0],matriz[1][1],matriz[2][2],matriz[3][3],matriz[4][4])
